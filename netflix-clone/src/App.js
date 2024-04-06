b3f05324006825ee3bdde7810a5e68156b724f7b
Abrham yeshitla <107958643+Abrshi@users.noreply.github.com>
2024-04-06 11:46:00 +0300

    hadder and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-    <h2>heloo</h2>
+    <Home />
     </div>
   );
 }
---
+++ netflix-clone/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../component/footer/Footer';
+import Header from '../component/header/Header';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
