7fa35d08fac7016bf832a5b2adcc5b953b42e0c4
Abrham yeshitla <107958643+Abrshi@users.noreply.github.com>
2024-04-13 12:20:49 +0300

    add baner

--- netflix-clone/src/pages/Home.js
+++ netflix-clone/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from '../component/footer/Footer';
 import Header from '../component/header/Header';
-
+import Banner from '../component/Banner/Banner.js';
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
